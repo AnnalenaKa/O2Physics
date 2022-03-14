64e57114569a6e437df57ecf359fb97445766423
AnnalenaKa <annalena.kalteyer@gmail.com>
2022-03-14 14:06:01 +0100

    Remove TFormula use cpluplus function

--- Common/Core/PID/TPCPIDResponse.h
+++ Common/Core/PID/TPCPIDResponse.h
@@ -20,10 +20,7 @@
 #include <array>
 #include <vector>
 #include <cmath>
-#include <iostream>
 #include "Framework/Logger.h"
-// ROOT includes
-#include "TFormula.h"
 // O2 includes
 #include "ReconstructionDataFormats/PID.h"
 #include "DataFormatsTPC/BetheBlochAleph.h"
@@ -42,28 +39,30 @@ class Response
 
   /// Setter and Getter for the private parameters
   void SetBetheBlochParams(const std::array<float, 5>& betheBlochParams) { mBetheBlochParams = betheBlochParams; }
+  void SetResolutionParamsDefault(const std::array<float, 2>& resolutionParamsDefault) { mResolutionParamsDefault = resolutionParamsDefault; }
   void SetResolutionParams(const std::vector<double>& resolutionParams) { mResolutionParams = resolutionParams; }
   void SetMIP(const float mip) { mMIP = mip; }
   void SetChargeFactor(const float chargeFactor) { mChargeFactor = chargeFactor; }
   void SetMultiplicityNormalization(const float multNormalization) { mMultNormalization = multNormalization; }
-  void SetResolutionParametrization(TFormula* sigmaParametrization) { mSigmaParametrization.reset(sigmaParametrization); }
   void SetUseDefaultResolutionParam(const bool useDefault) { mUseDefaultResolutionParam = useDefault; }
-  void SetParameters(Response* response)
+  void SetParameters(const Response* response)
   {
     mBetheBlochParams = response->GetBetheBlochParams();
+    mResolutionParamsDefault = response->GetResolutionParamsDefault();
     mResolutionParams = response->GetResolutionParams();
     mMIP = response->GetMIP();
     mChargeFactor = response->GetChargeFactor();
     mMultNormalization = response->GetMultiplicityNormalization();
+    mUseDefaultResolutionParam = response->GetUseDefaultResolutionParam();
   }
 
   const std::array<float, 5> GetBetheBlochParams() const { return mBetheBlochParams; }
+  const std::array<float, 2> GetResolutionParamsDefault() const { return mResolutionParamsDefault; }
   const std::vector<double> GetResolutionParams() const { return mResolutionParams; }
   const float GetMIP() const { return mMIP; }
   const float GetChargeFactor() const { return mChargeFactor; }
   const float GetMultiplicityNormalization() const { return mMultNormalization; }
-
-  // std::unique_ptr<TFormula> GetResolutionParametrization() { return mSigmaParametrization; }
+  const bool GetUseDefaultResolutionParam() const { return mUseDefaultResolutionParam; }
 
   /// Gets the expected signal of the track
   template <typename TrackType>
@@ -90,7 +89,6 @@ class Response
   float mChargeFactor = 2.299999952316284f;
   float mMultNormalization = 11000.;
   bool mUseDefaultResolutionParam = true;
-  std::unique_ptr<TFormula> mSigmaParametrization{new TFormula("fSigmaParametrization", "sqrt(([0]**2)*x[0]+(([1]**2)*(x[2]*[5])*(x[0]/sqrt(1+x[1]**2))**[2])+x[2]*x[3]**2+([4]*x[4])**2 +((x[5]*[6])**2)+(x[5]*(x[0]/sqrt(1+x[1]**2))*[7])**2)")};
 
   ClassDefNV(Response, 2);
 
@@ -123,7 +121,7 @@ inline float Response::GetExpectedSigma(const CollisionType& collision, const Tr
 
     const std::vector<double> values{1.f / dEdx, track.tgl(), std::sqrt(ncl), relReso, track.signed1Pt(), collision.multTPC() / mMultNormalization};
 
-    const float reso = mSigmaParametrization->EvalPar(values.data(), mResolutionParams.data()) * dEdx * mMIP;
+    const float reso = sqrt(pow(mResolutionParams[0], 2) * values[0] + pow(mResolutionParams[1], 2) * (values[2] * mResolutionParams[5]) * pow(values[0] / sqrt(1 + pow(values[1], 2)), mResolutionParams[2]) + values[2] * pow(values[3], 2) + pow(mResolutionParams[4] * values[4], 2) + pow(values[5] * mResolutionParams[6], 2) + pow(values[5] * (values[0] / sqrt(1 + pow(values[1], 2))) * mResolutionParams[7], 2)) * dEdx * mMIP;
     reso >= 0.f ? resolution = reso : resolution = 0.f;
   }
   return resolution;
